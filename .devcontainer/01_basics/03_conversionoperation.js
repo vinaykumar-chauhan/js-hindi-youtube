534e91f472384c751b718946cb37b1b3ba27cbfb
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-24 14:05:26 +0000

    operations

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -19,7 +19,7 @@ let isLoggedIn = 0;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -28,5 +28,43 @@ console.log(booleanIsLoggedIn);
 let sum = 33;
 
 let stringNumber = String(sum);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***************                  Operations                       ****************** //
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " vinay";
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2 , num3 ;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+
+console.log(gameCounter);
