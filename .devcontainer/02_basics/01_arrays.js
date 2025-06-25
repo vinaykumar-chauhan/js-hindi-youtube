0847bf963c62a4e2626c8ea0ccc2c90fd56f91e9
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-25 10:41:41 +0000

    basics methods

--- .devcontainer/02_basics/01_arrays.js
+++ .devcontainer/02_basics/01_arrays.js
@@ -38,3 +38,42 @@ const myn2 = myArray.splice(1,3);
 // console.log(myn2);
 // console.log("C ", myArray);
 
+
+
+// *************************    Array Basics  ***************************
+
+
+const marvel_Heroes = ["ironman", "spiderman","hulk","captain america","thor"];
+
+const dc_Heroes = ["superman","batman","flash"];
+
+// marvel_Heroes.push(dc_Heroes);
+
+// console.log(marvel_Heroes);
+// console.log(marvel_Heroes[5][2]);
+
+// const allHeroes = marvel_Heroes.concat(dc_Heroes);
+// console.log(allHeroes);
+
+const all_New_Heroes = [...marvel_Heroes, ...dc_Heroes];
+// console.log(all_New_Heroes);
+
+const anotherArr = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+
+const real_another_Arr = anotherArr.flat(Infinity);
+// console.log(real_another_Arr);
+
+// console.log(Array.isArray("vinay"));
+// console.log(Array.from("vinay"));
+// console.log(Array.from({name : "vinay"}));    // intersting 
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+
+// console.log(Array.of(score1,score2,score3));
+
+
+
+
+
