1849c6c21135a0dbf628b044b0e581abf20f01f5
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-25 10:10:13 +0000

    array methods

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,40 @@
+// Array
+
+const myArray = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman", "nagraj", "krish","chotabheem"];
+
+const myArr2 = new Array(1,2,3,4);
+ 
+// console.log(myArray[1]);
+
+//   Array Methods
+
+// myArray.push(6);
+// myArray.push(7);
+// myArray.pop();
+
+// myArray.unshift(9);
+// myArray.shift(9);
+
+// console.log(myArray.includes(4));
+// console.log(myArray.indexOf(4));
+
+const newArr = myArray.join();
+
+// console.log(myArray);
+// console.log(typeof newArr);
+
+//slice , splice
+
+// console.log("A ", myArray);
+
+const myn1 = myArray.slice(1,3);
+
+// console.log(myn1);
+// console.log("B ", myArray);
+
+const myn2 = myArray.splice(1,3);
+
+// console.log(myn2);
+// console.log("C ", myArray);
+
