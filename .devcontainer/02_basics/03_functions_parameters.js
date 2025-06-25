4c544e34eebc6815ae42378d8c1cdf7be08436a9
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-25 20:06:45 +0000

    functions

---
+++ .devcontainer/02_basics/03_functions_parameters.js
@@ -0,0 +1,15 @@
+function sayMyName() {
+    console.log("v");
+    console.log("i");
+    console.log("n");
+    console.log("a");
+    console.log("y");
+}
+
+// sayMyName();
+
+function addTwoNumbers(num1,num2){
+    console.log(num1+num2);
+    }
+
+    addTwoNumbers(3,null);
