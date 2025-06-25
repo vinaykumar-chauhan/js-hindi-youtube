0fa03d72c0bc92dabb1bd8a750b46354723fc1df
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-25 03:47:05 +0000

    number methods

---
+++ .devcontainer/01_basics/07_nums_and_math.js
@@ -0,0 +1,12 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.456;
+console.log(otherNumber.toPrecision(3));
+
