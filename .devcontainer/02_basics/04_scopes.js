d13357380d612ff3c10ea9ccf065ebccf57083ac
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-26 08:41:11 +0000

    expression function

--- .devcontainer/02_basics/04_scopes.js
+++ .devcontainer/02_basics/04_scopes.js
@@ -14,8 +14,52 @@ var c = 30;
 
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
 //-----------------------------------------------------
+
+// nested scope
+
+// const result = "99%";
+function one(){
+     const username = "vinay";
+
+     function two(){
+        const website = "youtube";
+        console.log(username)
+        // console.log(result);
+        
+     }
+//   console.log(website);
+  
+//   two();
+}
+
+one();
+
+if(true){
+    const username = "vinay";
+    if(username === "vinay"){
+       const website = " youtube";
+    //    console.log(username + website); 
+           }
+        //    console.log(website);                          // block scope variable
+}
+// console.log(username);                                    // block scope variable
+
+// ----------------  interesting  -------------------------
+
+addone(5);
+function addone(num){
+   return num + 1;
+}
+
+// addTwo(5);                                             // error declaration 
+const addTwo = function(num){                          // expression function
+    return num + 2;
+}
+
+
+
