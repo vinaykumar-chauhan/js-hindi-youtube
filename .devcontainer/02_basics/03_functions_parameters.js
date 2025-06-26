73e5f1703053b1dd59f8065f7babafbc0cfb6f92
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-26 06:12:17 +0000

    parameters and arguments

--- .devcontainer/02_basics/03_functions_parameters.js
+++ .devcontainer/02_basics/03_functions_parameters.js
@@ -8,8 +8,35 @@ function sayMyName() {
 
 // sayMyName();
 
+// function addTwoNumbers(num1,num2){
+//     console.log(num1+num2);
+//     }
+
 function addTwoNumbers(num1,num2){
-    console.log(num1+num2);
+    // let result = num1+num2;
+    // return result;
+    return num1 + num2;
+
     }
 
+    const result = addTwoNumbers(3,4);
+    addTwoNumbers(3,"4");
     addTwoNumbers(3,null);
+    addTwoNumbers(3,undefined);
+    addTwoNumbers(3,true);
+    addTwoNumbers(3,false);
+
+    // console.log("Result:" , result);
+
+    function loginUserMessage(username = "sam"){
+        if(!username){
+            console.log("please enter a username");
+            return
+        }
+        return `${username} just logged in`
+    }
+    
+    // const logInUser = loginUserMessage("vinay");
+    const logInUser = loginUserMessage();
+    console.log(logInUser);
+    
