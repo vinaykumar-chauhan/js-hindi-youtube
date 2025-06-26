7ffbcf8cde73009adaeabec0deb7daba445c25b3
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-26 09:33:16 +0000

    iife

---
+++ .devcontainer/02_basics/06_iife.js
@@ -0,0 +1,22 @@
+// Immediately Invoked Function Expression (IIFE);
+
+
+// (function chai(){                               // named iife
+//     console.log("DB Connected one");    
+// })();
+
+// (function (){                                   
+//     console.log("DB Connected one");    
+// })();
+
+// ( () => {
+//     console.log("DB Connected Two");    
+// }) ();
+
+// ( (name) => {                                      // iife with parameters and arguments
+//     console.log(`DB Connected Two ${name}`);    
+// }) ("vinay");
+
+// ( async () => {
+//     console.log("DB Connected async");    
+// }) ();
