adfba7b7d37efc598ee2d3a06bbacab32c048bed
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-26 08:10:57 +0000

    global scope

---
+++ .devcontainer/02_basics/04_scopes.js
@@ -0,0 +1,21 @@
+// global scope
+
+var c = 300;
+let a = 100;
+
+
+if(true){
+
+let a = 10;
+// console.log("INNER :" , a);
+
+const b = 20;
+var c = 30;
+
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+//-----------------------------------------------------
