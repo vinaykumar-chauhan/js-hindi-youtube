de25c459f494d0855f9b0fdd5a2ddc04e1b60bad
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-27 16:20:11 +0000

    do-while loop

--- .devcontainer/03_basics/03_iterations.js
+++ .devcontainer/03_basics/03_iterations.js
@@ -47,3 +47,28 @@ for (let index = 1; index <= 20; index++) {
     // console.log(`value of i is: ${index}`);    
     
 }
+
+// while loop 
+
+let index = 0;
+
+// while (index <= 10) {
+    // console.log(`Value of index is: ${index}`);
+    // index = index + 2;   
+// }
+
+// const myArray = ["flash" , "superman" , "batman"];
+
+// while (index < myArray.length) {
+//     console.log(`value is: ${myArray[index]}`);
+//     index = index + 1; 
+// }
+
+
+// do-while loop
+
+let score = 11;
+do {
+    // console.log(`Score is : ${score}`);
+    score++;
+} while (score <= 10);
