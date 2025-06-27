5d7e29ef9e2be840e412307cda2fa562f83f105c
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-27 13:45:02 +0000

    control flow in conditional statement

--- .devcontainer/02_basics/08_control_flow.js
+++ .devcontainer/02_basics/08_control_flow.js
@@ -4,3 +4,37 @@ if(true){
 
 }
 // < , > , <= , >= , == , != , === , !== ,
+
+
+// ---------------------------------
+
+// switch (key) {
+//      case value:
+
+//      break;
+
+//      default:
+//         breaak;
+// }
+
+const month = "march";
+
+switch (month) {                              //   switch case 
+     case 1:
+       console.log("january");
+     break;
+     case 2:
+       console.log("febuary");
+     break;
+     case 3:
+       console.log("march");
+     break;
+     case 4:
+       console.log("april");
+     break;
+     
+     default:
+        console.log("default case match");
+        
+        break;
+}
