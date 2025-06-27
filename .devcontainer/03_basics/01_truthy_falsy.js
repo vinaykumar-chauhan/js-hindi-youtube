3356a70999a6cfaf8eb2242bf47478ec73d00618
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-27 14:21:36 +0000

    truthy and false

---
+++ .devcontainer/03_basics/01_truthy_falsy.js
@@ -0,0 +1,46 @@
+// const userEmail = "vinay@gmail.com";
+const userEmail = [];
+
+// if(userEmail){
+//     console.log("get user email");
+//     }else {
+//         console.log("not get user email");
+//     }
+
+    /*
+    // falsy values
+    // false , 0 , -0 , BigInt 0n , "" , null , undefined , NaN
+    */
+
+    /*
+    //truthy values
+    // true , "0" , 'false' , " " , [] , {} , function(){} 
+    */
+
+    // if(userEmail.length === 0){
+    //     console.log("Array is empty");        
+    // }
+
+    const emptyObj = {};
+
+    // if(Object.keys(emptyObj).length === 0){           // Object.keys(emptyObj) converts into Array   
+    //     console.log("Object is empty");
+        
+    // }
+
+    // Nullish coalescing Operator (??): null undefined
+
+    let val1;
+    // val1 = 5 ?? 10;
+    // val1 = null ?? 10;                    //  Nullish coalescing Operator (??):
+    //   val1 = undefined ?? 15;
+    // val1 = null ?? 20 ?? 40;
+    // console.log(val1);
+    
+
+    // Ternary  Operators
+    // condition ? true : false ;
+
+    // const iceTeaPrice = 100;
+    // iceTeaPrice >= 80 ? console.log("iceTeaprice is greater than 80") : console.log("iceTeaprice is lesser than 80");
+    
