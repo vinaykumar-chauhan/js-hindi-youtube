a026b93a0c4af3f83d4bd8fdfe03d2d9604aa2b7
vinaykumar-chauhan <vinaymarineservices@gmail.com>
2025-06-28 16:29:37 +0000

    reduce

--- .devcontainer/03_basics/04_map_filter_reduce.js
+++ .devcontainer/03_basics/04_map_filter_reduce.js
@@ -43,5 +43,48 @@ const books = [
    userBooks = books.filter( (bk) => bk.publish >= 1995 && bk.genre === 'History');
     //  console.log(userBooks);
 
-    
+    ///--------------------------------------------------------------
+
+const myNumbers = [1 ,2,3,4,5,6,7,8,9,10];
+
+// const newNums =  myNumbers.map( (num) => num + 10);
+
+const newNums = myNumbers.map((num) => num * 10 ).map((num) => num + 1).filter((num) => num >= 40 )
+
+  // console.log(newNums);
+
+// ---------------------------------------------------
+
+const mynum = [1,2,3,4]
   
+const initialValue = 0;
+// const sumWithInitial = mynum.reduce((accumulator,currentValue) => accumulator + currentValue,initialValue);
+
+const sumWithInitial = mynum.reduce( function (acc , currval) {
+  console.log(`acc: ${acc} and currval: ${currval}`);
+  return acc + currval;
+  },3 )
+
+// console.log(sumWithInitial);
+
+const shoppingCart = [
+  {
+    itemName: "js course",
+    price: 2999
+  },
+  {
+    itemName: "python",
+    price: 1999
+  },
+  {
+    itemName: "mobile dev course",
+    price: 3000
+  },
+  {
+    itemName: "data science course",
+    price: 4000
+  },
+]
+
+const totalPrice = shoppingCart.reduce( (acc, item) => { return (acc + item.price)},0);
+// console.log(totalPrice);
